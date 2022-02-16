7e38b41bac090451d7629d89aa34e38c234c3cf7
Snow Lukin <everyoursden@gmail.com>
2022-02-16 19:21:54 +0300

    Initial Commit

---
+++ TrafficLightsLoop/TrafficLightsLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsLoopApp.swift
+//  TrafficLightsLoop
+//
+//  Created by Snow Lukin on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
